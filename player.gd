7a7c90fe2ae268404ce533eff2e9e903ddf90ba6
OliveADay <sharinggon23@gmail.com>
2026-03-05 13:38:09 -0800

    player controller

---
+++ player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+@export var SPEED = 300
+@export var ANGLE_SPEED = 0.1
+
+@export var ACCELERATION = 10
+@export var DECELERATION = 10
+var angular_velocity = 0
+
+
+func _physics_process(delta: float) -> void:
+	var horizontal=Input.get_axis("left","right")
+	var vertical = Input.get_axis("backward","forward")
+	if(vertical!=0):
+		velocity.x = move_toward(velocity.x,SPEED*cos(rotation)*vertical,ACCELERATION)
+		velocity.y = move_toward(velocity.y,SPEED*sin(rotation)*vertical,ACCELERATION)
+	else:
+		velocity.x = move_toward(velocity.x,0,DECELERATION)
+		velocity.y = move_toward(velocity.y,0,DECELERATION)
+	
+	if(horizontal!=0):
+		angular_velocity = move_toward(angular_velocity,ANGLE_SPEED*horizontal,ACCELERATION)
+	else:
+		angular_velocity = move_toward(angular_velocity, 0, DECELERATION)
+	
+	rotate(angular_velocity)
+		
+	move_and_slide()
