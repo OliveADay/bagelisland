465cf8ef4f5247aea486bb136975b9ed28f43172
OliveADay <sharinggon23@gmail.com>
2026-03-05 16:23:19 -0800

    accel change

--- player.gd
+++ player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 @export var SPEED = 300
 @export var ANGLE_SPEED = 0.1
 
-@export var ACCELERATION = 10
-@export var DECELERATION = 10
+@export var ACCELERATION = 3
+@export var DECELERATION = 3
 var angular_velocity = 0
 
 
@@ -18,7 +18,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y,0,DECELERATION)
 	
 	if(horizontal!=0):
-		angular_velocity = move_toward(angular_velocity,ANGLE_SPEED*horizontal,ACCELERATION)
+		angular_velocity = move_toward(angular_velocity,ANGLE_SPEED*horizontal,ACCELERATION/velocity.length())
 	else:
 		angular_velocity = move_toward(angular_velocity, 0, DECELERATION)
 	
